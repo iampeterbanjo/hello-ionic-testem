8a832fe7f260a6546cad9aaa04dbc5ff7b4664c2
iampeterbanjo <me@iampeterbanjo.com>
2014-06-17 21:13:43 +0100

    add passing test for account routes

---
+++ spec/routes/routes_spec.js
@@ -0,0 +1,13 @@
+﻿describe('routes in app', function () {
+	beforeEach(function () {
+		angular.mock.module('starter')
+	})
+
+	beforeEach(inject(function ($state) {
+		this.$state = $state
+	}))
+
+	it('should have a route to accounts', function () {
+		expect(this.$state.get('tab.account').url).toBe('/account')
+	})
+})
