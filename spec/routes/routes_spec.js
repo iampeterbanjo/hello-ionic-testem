4ed45d1959298817f391eb034f099e3ee2308d6a
iampeterbanjo <me@iampeterbanjo.com>
2014-06-17 22:30:40 +0100

    add test for playlists and playlist routes

--- spec/routes/routes_spec.js
+++ spec/routes/routes_spec.js
@@ -3,11 +3,35 @@
 		angular.mock.module('starter')
 	})
 
-	beforeEach(inject(function ($state) {
+	beforeEach(inject(function ($state, $urlRouter) {
 		this.$state = $state
+		this.$urlRouter = $urlRouter
 	}))
 
-	it('should have a route to accounts', function () {
-		expect(this.$state.get('tab.account').url).toBe('/account')
+	// Playlists
+
+	it('should have a route to playlists', function () {
+		expect(this.$state.get('app.playlists').url).toBe('/playlists')
 	})
+
+	it('should have playlists served by PlaylistsCtrl', function () {
+		expect(this.$state.get('app.playlists').views.menuContent.controller).toBe('PlaylistsCtrl')
+	})
+
+	// Playlist details
+
+	it('should have a route to playlist details', function () {
+		expect(this.$state.get('app.single').url).toBe('/playlists/:playlistId')
+	})
+
+	it('should have playlist details served by PlaylistsCtrl', function () {
+		expect(this.$state.get('app.single').views.menuContent.controller).toBe('PlaylistCtrl')
+	})
+
+	// default route
+
+	// TODO
+	//it('should default to the playlists route', function () {
+	//	expect(this.$urlRouter)
+	//})
 })
